12fe6bcc2d7f06bfa08c88b083c030b8e330cb88
aesense <senseandrey@gmail.com>
2022-05-02 19:13:43 +0300

    fix AdminController.cs

--- SportBooking.Presentation/Controllers/AdminController.cs
+++ SportBooking.Presentation/Controllers/AdminController.cs
@@ -8,21 +8,8 @@ namespace SportBooking.Presentation.Controllers;
 [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
 public class AdminController : Controller
 {
-    private readonly IMailService _mailService;
-
-    public AdminController(IMailService mailService)
-    {
-        _mailService = mailService;
-    }
-
     public IActionResult Index()
     {
         return View();
-    }
-    
-    public async Task<IActionResult> SendTestMail()
-    {
-        await _mailService.SendMailAsync("senseandrey@gmail.com", "test", "test");
-        return Ok();
     }
 }
